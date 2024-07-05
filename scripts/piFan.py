85f04233617efc126e3898092b2d974e76e09134
ub <registros@mailbox.org>
2024-07-05 14:27:28 +0200

    change .json

--- scripts/piFan.py
+++ scripts/piFan.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     gpio_pin = 27
-    ir_file = "test.json"
+    ir_file = "/home/pi/git/piFan/scripts/test.json"
     remote = piir.Remote(ir_file, gpio_pin)
     action = args.action.lower()
 
