6649ce4ffde27a35b855d3ba640b67d80052d36b
ub <registros@mailbox.org>
2024-07-08 19:31:04 +0200

    jobs page

--- piFan-app.py
+++ piFan-app.py
@@ -50,7 +50,8 @@ def jobs():
 @piFan.route('/delete_job/<job_id>', methods=['POST'])
 def delete_job(job_id):
     del_cron(job_id)
-    return redirect(url_for('index.html'))
+    #return redirect(url_for('jobs'))
+    return render_template('index.html', jobs=jobs)
 
 @piFan.route('/remote', methods=['GET', 'POST'])
 def remote():
